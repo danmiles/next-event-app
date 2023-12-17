12ebebb65f66f4087cc3a9a46ca8d26ace297391
danmiles <41701228+danmiles@users.noreply.github.com>
2023-12-17 19:20:36 +0100

    install shadcn/ui

--- src/sections/home/Header.tsx
+++ src/sections/home/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
+// Components
+import { Button } from '@/components/ui/button';
 export default function Header() {
   return (
     <header>
       <div className="container">
         <div className="flex items-center justify-center h-screen">
           <h1 className="h1">Next.js 14 + Tailwind Css Starter</h1>
+          <Button variant={'destructive'}>Click me</Button>
+            
         </div>
       </div>
     </header>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,45 +1,84 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/sections/**/*.{js,ts,jsx,tsx,mdx}',    
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   theme: {
     container: {
       center: true,
-      padding: '1rem',
-    },
-    screens: {
-      sm: '640px',
-      // => @media (min-width: 640px) { ... }
-
-      md: '768px',
-      // => @media (min-width: 768px) { ... }
-
-      lg: '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      xl: '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1440px',
-      // => @media (min-width: 1440px) { ... }
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
     extend: {
       colors: {
+        
         'prim-clr': 'var(--primary-color)',
         'sec-clr': 'var(--secondary-color)',
         'font-clr': 'var(--font-color)',
         'link-clr': 'var(--link-color)',
         'hover': 'var(--hover-color)',
         'white': 'var(--white-color)',
+        // shadcn/ui class
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  // end theme
-
-  plugins: [],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+}
