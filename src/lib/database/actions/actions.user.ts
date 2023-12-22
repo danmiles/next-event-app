08c818e4cff4aee5e601df7429d94d1f266e02f0
Denis <41701228+danmiles@users.noreply.github.com>
2023-12-22 16:17:21 +0100

    add types generated by ChatGBT

--- src/lib/database/actions/actions.user.ts
+++ src/lib/database/actions/actions.user.ts
@@ -1,1 +1,21 @@
-'use server'
+'use server';
+// Connect to MongoDB
+import { connectToDatabase } from '@/lib/database/index';
+// Models
+import User from '@/lib/database/models/user.model';
+// Types
+import { CreateUserParams } from '@/types/index';
+// handleError function
+import { handleError } from '@/lib/utils';
+
+export async function createUser(user: CreateUserParams) {
+  try {
+    await connectToDatabase();
+
+    const newUser = await User.create(user);
+    return JSON.parse(JSON.stringify(newUser))
+
+  } catch (error) {
+    handleError(error);
+  }
+}
