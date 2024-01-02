b781231ec4edf0cd7aa1f3e8d7ba6a407921d335
Denis <41701228+danmiles@users.noreply.github.com>
2024-01-02 23:29:13 +0100

    Update page.tsx

--- src/app/(root)/events/[id]/update/page.tsx
+++ src/app/(root)/events/[id]/update/page.tsx
@@ -1,18 +1,35 @@
-import EventForm from '@/components/shared/EventForm';
-import { auth } from '@clerk/nextjs';
-import React from 'react';
+import EventForm from "@/components/shared/EventForm"
+import { getEventById } from "@/lib/actions/event.actions"
+import { auth } from "@clerk/nextjs";
 
-export default function UpdateEnent() {
+type UpdateEventProps = {
+  params: {
+    id: string
+  }
+}
+
+const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
   const { sessionClaims } = auth();
 
   const userId = sessionClaims?.userId as string;
+  const event = await getEventById(id)
+
   return (
-    <section className=" bg-dotted-pattern bg-slate-100 bg-cover bg-center py-5 md:py-10">
-      <h3 className="wrapper h3-bold text-center sm:text-left">Update Event</h3>
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">Update Event</h3>
+      </section>
 
       <div className="wrapper my-8">
-        <EventForm userId={userId} type="Update" />
+        <EventForm 
+          type="Update" 
+          event={event} 
+          eventId={event._id} 
+          userId={userId} 
+        />
       </div>
-    </section>
-  );
+    </>
+  )
 }
+
+export default UpdateEvent
