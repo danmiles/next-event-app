9a0c662d1403f90323b85bbeadd306ee5a7d6993
Denis <41701228+danmiles@users.noreply.github.com>
2024-01-07 20:09:46 +0100

    Update page.tsx

--- src/app/(root)/events/[id]/page.tsx
+++ src/app/(root)/events/[id]/page.tsx
@@ -28,8 +28,8 @@ export default async function EventDetails({
             src={event.imageUrl}
             alt="hero image"
             width={1000}
-            height={1000}
-            className="h-full min-h-[300px] object-cover object-center"
+            height={800}
+            className="h-full min-h-[400px] object-contain"
           />
 
           <div className="flex w-full flex-col gap-8 p-5 md:p-10">
