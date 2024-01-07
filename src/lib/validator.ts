3fc7c1482289c43f9070ec1b57a9e88d9555c56a
Denis <41701228+danmiles@users.noreply.github.com>
2024-01-07 23:09:32 +0100

    Update validator.ts

--- src/lib/validator.ts
+++ src/lib/validator.ts
@@ -10,10 +10,10 @@ export const eventFormSchema = z.object({
     .string()
     .min(3, 'Location must be at least 3 characters')
     .max(500, 'Location must be less than 800 characters'),
-  imageUrl: z.string().nonempty('Please select an image for your event'),
-  startDateTime: z.date(),
-  endDateTime: z.date(),
-  categoryId: z.string().nonempty('Please select an category for your event'),
+  imageUrl: z.string().min(1,'Please select an image for your event'),
+  startDateTime: z.date().min(new Date(), 'Start date must be in the future'),
+  endDateTime: z.date().min(new Date(), 'End date must be in the future'),
+  categoryId: z.string().min(1,'Please select an category for your event or create a new one'),
   price: z.string(),
   isFree: z.boolean(),
   url: z.string().url(),
